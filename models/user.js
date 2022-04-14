44d6e50e5ae8e4c6000de56fbf637f8bd7089ec1
Tomy Visouthiphongs <tvisouthiphong@gmail.com>
2022-04-14 16:26:22 +0200

    views - Changed modal message in basket page

--- models/user.js
+++ models/user.js
@@ -4,7 +4,8 @@ var userSchema = mongoose.Schema({
     name: String,
     firstName: String,
     email: String,
-    password: String
+    password: String,
+    journeys: [{ type: mongoose.Schema.Types.ObjectId, ref:'journeys' }]
   });
   
   var userModel = mongoose.model('user', userSchema);
